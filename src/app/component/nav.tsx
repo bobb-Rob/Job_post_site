925e17ce29cb1828a67cc5d6e80de14c799e7cfc
Robertson Akpan <robertsonakpan@gmail.com>
2023-06-28 22:58:34 +0100

    Navbar logic completed

--- src/app/component/nav.tsx
+++ src/app/component/nav.tsx
@@ -1,13 +1,27 @@
 'use client'
  import * as React from 'react';
  import { MdOutlineWorkOutline } from 'react-icons/md';
- import { useState } from 'react';
- 
- export interface NavProps {
- }
- 
- export default function Nav (props: NavProps) {
+ import { useState, useEffect } from 'react';
+  
+ export default function Nav () {
   const [isFocus, setIsFocus] = useState(false);
+  const [placeholder, setPlaceholder] = useState('Title, companies, expertise or benefits');
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mobilePlaceholder = 'Title, companies, expertise...';
+      const isMobileView = window.innerWidth < 768;
+
+      setPlaceholder(isMobileView ? mobilePlaceholder : 'Title, companies, expertise or benefits');
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleFocus = () => {
     setIsFocus(true);
@@ -20,16 +34,16 @@
   return (
     <header>      
       <div
-        className="bg-[url('/backgroundImg.png')] rounded-lg flex justify-center items-center p-4 h-[138px]"        
+        className="bg-[url('/backgroundImg.png')] bg-cover rounded-lg flex justify-center items-center p-4 h-[138px]"        
       >
-        <div className="bg-white rounded flex justify-between items-center p-2 pr-1 h-14 w-4/5 md:w-2/3">
+        <div className="bg-white rounded flex justify-between items-center p-2 pr-1 h-14 w-[95%] md:w-2/3">
           { isFocus ? null : <MdOutlineWorkOutline
             className="text-xl mr-2 text-myGray"
           /> }
           <input
-            className="h-full w-full roboto text-xs"
+            className="h-full w-full roboto text-xs focus:outline-none"
             type="text"
-            placeholder='Title, companies, expertise or benefits'
+            placeholder={placeholder}
             onFocus={handleFocus}
             onBlur={handleBlur}
           />
